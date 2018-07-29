0b57cb643729b42831513e9da69049db88d22f27
HaHa21 <domlee2012@gmail.com>
2018-07-29 22:53:38 +0100

    changes for frontend and backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AuthService } from './auth/auth.service';
+import { RouterModule } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatButtonModule,  MatProgressSpinnerModule, MatCardModule, MatToolbarModule, MatInputModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './auth/header/header.component';
+import { RegisterComponent } from "./auth/signup/register.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+
+    MatButtonModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [ AuthService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
